2e6daa34527c2da5035b0d3fed7244a74288badc
ariascripts <148398428+ariascripts@users.noreply.github.com>
2024-02-16 18:16:07 -0500

    Update SummoningTaverleyShop.lua
    Removed UTILS dependency
    Made bot use notepaper since I couldn't find a portable deposit box

--- SummoningTaverleyShop.lua
+++ SummoningTaverleyShop.lua
@@ -1,23 +1,45 @@
---[[IMPORTANT: Make sure your bank is full and:
+--[[IMPORTANT: If you are using a portable deposit box, make sure your bank is full and:
     -contains the pouch you are making
     -does not contain any items that you don't want to deposit (i.e. spirit shards, the familiar's charm/secondary)
 ]]
 --local API = require("libraries.api")
---local UTILS = require("libraries.utils")
 --local Shop = require("libraries.interfaces.Shop")
 local API = require("api")
-local UTILS = require("utils")
 local Shop = require("Shop")
 
 local SUMMONING_SHOP = WPOINT.new(2931, 3448, 0)
 
 local familiars = {
-    {name = "Geyser titan", level = 89, pouch_id = 12786, charm = "Blue charm", secondary = "Water talisman",
-        secondary_unnoted = 1444, secondary_noted = 1445, sellMethod = true},
-    {name = "Fire titan", level = 79, pouch_id = 12802, charm = "Blue charm", secondary = "Fire talisman",
-        secondary_unnoted = 1442, secondary_noted = 1443, sellMethod = true},
-    {name = "Spirit jelly", level = 55, pouch_id = 12027, charm = "Blue charm", secondary = "Jug of water",
-        secondary_unnoted = 1937, secondary_noted = 1938, sellMethod = true},
+    {
+        name = "Geyser titan",
+        level = 89,
+        pouch_id = 12786,
+        charm = "Blue charm",
+        secondary = "Water talisman",
+        secondary_unnoted = 1444,
+        secondary_noted = 1445,
+        sellMethod = true
+    },
+    {
+        name = "Fire titan",
+        level = 79,
+        pouch_id = 12802,
+        charm = "Blue charm",
+        secondary = "Fire talisman",
+        secondary_unnoted = 1442,
+        secondary_noted = 1443,
+        sellMethod = true
+    },
+    {
+        name = "Spirit jelly",
+        level = 55,
+        pouch_id = 12027,
+        charm = "Blue charm",
+        secondary = "Jug of water",
+        secondary_unnoted = 1937,
+        secondary_noted = 1938,
+        sellMethod = true
+    },
 }
 
 local curr = familiars[1]
@@ -39,17 +61,33 @@ local function getSelectedTab()
 end
 
 local function getCreationInterfaceSelectedItemID()
-    return API.VB_FindPSett(1170, 1, 0).state
+    return API.VB_FindPSett(1170, 0, 0).state
+end
+
+local function getSelectedItemIndex()
+    return API.VB_FindPSett(1031, 1, 0).state
 end
 
 local function isCreationInterfaceOpen()
-    return getCreationInterfaceSelectedItemID() ~= -1
+    return getCreationInterfaceSelectedItemID() ~= -1 and getCreationInterfaceSelectedItemID() ~= 2 ^ 31 - 1
+end
+
+local function distance(tile)
+    if tile == nil then
+        print("Distance called on nil tile")
+        return 1000000007
+    end
+
+    local player = API.PlayerCoord()
+    local x = player.x - tile.x
+    local y = player.y - tile.y
+    return math.sqrt(x * x + y * y)
 end
 
 API.Write_LoopyLoop(true)
 while API.Read_LoopyLoop() do
     if API.InvItemcount_1(curr.pouch_id) > 0 then
-        local boxes = getDepositBoxes()
+        --[[local boxes = getDepositBoxes()
         if #boxes > 0 then
             if API.Compare2874Status(69, false) then
                 API.DoAction_Interface(0x24,0xffffffff,1,11,5,-1,API.OFF_ACT_GeneralInterface_route) --deposit inventory button in portable deposit box
@@ -60,17 +98,38 @@ while API.Read_LoopyLoop() do
         else
             print("No boxes detected")
             API.RandomSleep2(600, 100, 100)
+        end]]
+        local vec = API.ReadInvArrays33()
+        if getSelectedItemIndex() > 0 then
+            print("Using notepaper on pouch")
+            for i = 1, #vec do
+                if vec[i].itemid1 == curr.pouch_id then
+                    API.DoAction_Interface(0x24, vec[i].itemid1, 0, 1473, 5, vec[i].index,
+                        API.OFF_ACT_GeneralInterface_route1)
+                    API.RandomSleep2(600, 100, 100)
+                    break
+                end
+            end
+        else
+            for i = 1, #vec do
+                if vec[i].itemid1 > 0 and string.find(vec[i].textitem, "Magic notepaper") then
+                    API.DoAction_Interface(0x24, vec[i].itemid1, 0, 1473, 5, vec[i].index,
+                        API.OFF_ACT_Bladed_interface_route)
+                    break
+                end
+            end
         end
-    elseif UTILS.distance(SUMMONING_SHOP) >= 1 then
+    elseif distance(SUMMONING_SHOP) >= 1 then
         print("Walking next to obelisk")
         API.DoAction_Tile(SUMMONING_SHOP)
         API.RandomSleep2(600, 100, 100)
     elseif API.InvItemcount_1(curr.secondary_unnoted) >= 20 then
         if isCreationInterfaceOpen() then
-            API.KeyboardPress32(0x20,0) --press Space
+            API.KeyboardPress32(0x20, 0) --press Space
             API.RandomSleep2(300, 300, 300)
         else
-            if API.DoAction_Object1(0x29,0,{ 67036 },50) then
+            print("Interacting with obelisk")
+            if API.DoAction_Object1(0x29, 0, { 67036 }, 50) then
                 API.RandomSleep2(600, 100, 100)
             end
         end
@@ -80,12 +139,12 @@ while API.Read_LoopyLoop() do
                 print("Buying " .. curr.secondary)
                 Shop.buyId(curr.secondary_unnoted, Shop.BUY_OPTIONS.ALL)
             else
-                API.DoAction_Interface(0x24,0xffffffff,1,1265,32,-1,API.OFF_ACT_GeneralInterface_route) --switch to sell tab
+                API.DoAction_Interface(0x24, 0xffffffff, 1, 1265, 32, -1, API.OFF_ACT_GeneralInterface_route) --switch to sell tab
             end
         else
             print("Selling " .. curr.secondary)
             if getSelectedTab() == 0 then
-                API.DoAction_Interface(0x24,0xffffffff,1,1265,32,-1,API.OFF_ACT_GeneralInterface_route) --switch to sell tab
+                API.DoAction_Interface(0x24, 0xffffffff, 1, 1265, 32, -1, API.OFF_ACT_GeneralInterface_route) --switch to sell tab
             else
                 local items = API.ReadInvArrays33()
                 local index = -2
@@ -99,9 +158,9 @@ while API.Read_LoopyLoop() do
                     return
                 end
 
-                API.DoAction_Interface(0xffffffff,0xffffffff,6,1265,20,index,API.OFF_ACT_GeneralInterface_route) --Sell 500
+                API.DoAction_Interface(0xffffffff, 0xffffffff, 6, 1265, 20, index, API.OFF_ACT_GeneralInterface_route) --Sell 500
                 API.RandomSleep2(300, 300, 300)
-                API.DoAction_Interface(0x24,0xffffffff,1,1265,41,-1,API.OFF_ACT_GeneralInterface_route) --switch to buy tab
+                API.DoAction_Interface(0x24, 0xffffffff, 1, 1265, 41, -1, API.OFF_ACT_GeneralInterface_route)    --switch to buy tab
                 local f = function()
                     return getSelectedTab() == 0
                 end
@@ -109,7 +168,7 @@ while API.Read_LoopyLoop() do
             end
         end
     else
-        if API.DoAction_NPC(0x29,API.OFF_ACT_InteractNPC_route,{ 14866 },50) then
+        if API.DoAction_NPC(0x29, API.OFF_ACT_InteractNPC_route, { 14866 }, 50) then
             API.RandomSleep2(600, 100, 100)
         end
     end
